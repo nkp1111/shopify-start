6ce6e0e2bed4006e80ac99ba3a6791325f6f0586
nkp1111 <neerajp4321@gmail.com>
2023-05-23 16:55:12 +0530

    feat: add cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const cors = require("cors")
 const { ApolloServer } = require("@apollo/server")
 const { expressMiddleware } = require("@apollo/server/express4")
 
@@ -11,9 +12,9 @@ const app = express()
 
 const port = process.env.PORT || 4000
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
 
 const startApolloServer = async () => {
   const server = new ApolloServer({
